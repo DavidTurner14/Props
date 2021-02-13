dd8c3a2a0ffc166c7444795d1ccb68b18981c40e
DavidTurner14 <51490155+DavidTurner14@users.noreply.github.com>
2021-02-13 15:45:37 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      alt="avatar_img"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+    <input id="fName" placeholder="First Name" />
+  </div>,
+  document.getElementById("root")
+);
